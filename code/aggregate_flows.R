14d3f7fdbd0b9fe7e3e8e6cb6ac12bde51fa3511
Austin Kennedy <akenns1947@gmail.com>
2024-01-29 14:23:52 -0700

    Added baseline shares for SDID

--- code/aggregate_flows.R
+++ code/aggregate_flows.R
@@ -9,6 +9,9 @@ library(data.table)
 # library(abind)
 source('functions.R')
 
+#global options
+baseline <- '2019-01-01'
+
 trades_matched <- vroom('../temporary/matched_paxful_trades.csv')
 trades_matched$date <- as.POSIXct(trades_matched$date, format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
 
@@ -25,10 +28,32 @@ outflows <- flows_balanced %>%
   group_by(user_cc, time) %>%
   summarize(outflow = sum(volume))
 
-#prepare SDID data
-outflows$time_number <- as.numeric(as.factor(outflows$time))
-outflows$country_number <- as.numeric(as.factor(outflows$user_cc))
+###########prepare SDID data
+outflows_sdid <- data.frame(outflows)
+outflows_sdid$time_number <- as.numeric(as.factor(outflows$time))
+outflows_sdid$country_number <- as.numeric(as.factor(outflows$user_cc))
+
+#get baseline shares
+outflows_baseline <- getFlows(trades_matched, amount_usd, 'year') %>%
+  filter(user_cc != user_cc2,
+         time == baseline)
+
+
+outflows_baseline <- balanceFlows(outflows_baseline)
+
+shares_baseline <- outflows_baseline %>%
+  group_by(time, user_cc) %>%
+  mutate(share = volume/sum(volume)) %>%
+  select(-c(volume))
+
+shares_wide <- shares_baseline %>%
+  pivot_wider(names_from = user_cc2, values_from = share) %>%
+  ungroup() %>%
+  select(-c(time))
+
+outflows_sdid <- outflows_sdid %>% left_join(shares_wide, by = 'user_cc')
 
 #export data
-write.csv(flows_balanced, '../temporary/flows_balanced.csv', row.names = FALSE)
+write.csv(flows_balanced, '../temporary/bilateral_flows_balanced.csv', row.names = FALSE)
 write.csv(outflows, '../temporary/outflows_balanced.csv', row.names = FALSE)
+write.csv(outflows_sdid, '../temporary/data_sdid.csv', row.names = FALSE)
